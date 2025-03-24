ffddfa2081a219a46116358daf27c3e12f609391
Gowtamy Reddy Godhala <142113089+gowtamyreddy@users.noreply.github.com>
2025-03-23 23:46:34 -0400

    Update image_caption.py

--- pages/image_caption.py
+++ pages/image_caption.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from PIL import Image
 
-# Load API key
-load_dotenv()
-API_KEY = os.getenv("GEMINI_API_KEY")
+# ✅ Use API key from Streamlit secrets
+API_KEY = st.secrets["GEMINI_API_KEY"]
 
 # Initialize Client
 client = genai.Client(api_key=API_KEY)
@@ -17,16 +14,16 @@ uploaded_img = st.file_uploader("Generating the caption of the img")
 
 if uploaded_img:
     image = Image.open(uploaded_img)
-    st.image(image,caption = "Uploaded imgage")
+    st.image(image, caption="Uploaded image")
 
     if st.button("Generate Caption"):
         try:
-            with st.spinner("Generating Image Capion ..."):
-
+            with st.spinner("Generating Image Caption ..."):
                 response = client.models.generate_content(
-                model="gemini-2.0-flash-exp",
-                contents=["What is this image?",image])
-                st.subheader("Generated Caption ")   
+                    model="gemini-2.0-flash-exp",
+                    contents=["What is this image?", image]
+                )
+                st.subheader("Generated Caption")
                 st.write(response.text)
 
         except Exception as e:
